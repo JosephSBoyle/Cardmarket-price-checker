c56043881a509cd4e9058c4188e2fa65b30b478c
Joe Boyle <joe.boyle@blackcowtech.uk>
2022-11-05 16:32:02 +0000

    Add an dataframe extractor for marketplace offers (they are formatted differently to ones listed on a users personal page)

--- src/offers.py
+++ src/offers.py
@@ -1,6 +1,4 @@
-"""Module for extracting a user's cardmarket offers based on their unique cardmarket name.
-
-"""
+"""Module for extracting a user's cardmarket offers based on their unique cardmarket name."""
 import pandas as pd
 import requests
 import bs4
@@ -8,28 +6,23 @@ from decimal import Decimal
 from re import sub
 
 
-def _euro_string_to_decimal(string: str) -> Decimal:
+def _euro_money_to_decimal(string: str) -> Decimal:
     stripped = sub(r"[^\d,]", "", string)  # "0,20 €" -> "0,20"
-    stripped = stripped.replace(
-        ",",
-        ".",
-    )  # "0,20" -> "0.20"
+    stripped = stripped.replace(",", ".")  # "0,20" -> "0.20"
     return Decimal(stripped)
 
 
-def extract(user_id: str) -> pd.DataFrame:
+def extract(url: str) -> pd.DataFrame:
     """Extract a dataframe containing each of a user's offers.
 
-    - TODO Add a column linking to the product page with offers for the same item.
     - TODO find the next page link and recursively append to the pandas dataframe.
     """
-    url = "https://www.cardmarket.com/en/Magic/Users/" + user_id + "/Offers/Singles"
 
     res = requests.get(url)
     assert res.status_code == 200, "panic: request failed"
-    
+
     soup = bs4.BeautifulSoup(res.content, "lxml")
-    
+
     # Match all rows in the table.
     # We can't use `pd.read_table` unfortunately since the table doesn't use the correct <table>, <th>, <td> encoding
     rows = soup.find_all("div", {"class": "row no-gutters article-row"})
@@ -40,15 +33,51 @@ def extract(user_id: str) -> pd.DataFrame:
         # ['CardName', 'condition (e.g NM | PO | EX ), 'n avail.', '0.20 $', '0.20', '2']
         row_values = list(row.stripped_strings)
 
+        # TODO get rid of magic number: use a helper that filters on conditions perhaps?
+        # It's important to ensure this link is correct, we can create a validate row method perhaps.
+        product_url = row.find_all("a")[0]["href"]
         table_values.append(
             {
                 "name": row_values[0],
                 "cond": row_values[1],
-                "price": _euro_string_to_decimal(row_values[-2]),
+                "price": _euro_money_to_decimal(row_values[-2]),
                 "avail": int(row_values[-1]),
+                "marketplace_url": product_url,
             }
         )
 
-    table = pd.DataFrame(table_values, columns=("name", "cond", "price", "avail"))
-    table["avail"]
-    return table
+    return pd.DataFrame(table_values)
+
+
+def extract_market_offers(url: str) -> pd.DataFrame:
+    res = requests.get(url)
+    assert res.status_code == 200, "panic: request failed"
+
+    soup = bs4.BeautifulSoup(res.content, "lxml")
+
+    # Match all rows in the table.
+    # We can't use `pd.read_table` unfortunately since the table doesn't use the correct <table>, <th>, <td> encoding
+    rows = soup.find_all("div", {"class": "row no-gutters article-row"})
+
+    table_values = []
+    for row in rows:
+        # list(row.stripped_strings) example:
+        # ['207', 'MarinaMagic', 'PO', 'Realmete ex pero marcas en la parte e ariba', '1 avail.', '0,02 €', '0,02 €', '1']
+        row_values = list(row.stripped_strings)
+
+        # TODO get rid of magic number: use a helper that filters on conditions perhaps?
+        # It's important to ensure this link is correct, we can create a validate row method perhaps.
+        user_uri = row.find_all("a")[0]["href"]
+        table_values.append(
+            {
+                "cond": row_values[2],
+                "custom_text": row_values[3],
+                "price": _euro_money_to_decimal(row_values[-2]),
+                "avail": int(row_values[-1]),
+                "user_name": row_values[1],
+                "user_sales": int(row_values[0]),
+                "user_uri": user_uri,
+            }
+        )
+
+    return pd.DataFrame(table_values)
---
+++ tests/test_offers.py
@@ -0,0 +1,45 @@
+from src.offers import extract, extract_market_offers
+
+import pandas as pd
+
+
+def test_extract_from_users_offers_page():
+    """Test extracting offers from user's page into a pandas dataframe."""
+
+    test_username = "Extasia1"
+    url = "https://www.cardmarket.com/en/Magic/Users/" + test_username + "/Offers/Singles"
+
+    df = extract(url)
+    assert isinstance(df, pd.DataFrame)
+
+    assert df.name.dtype == object  # str
+    assert df.cond.dtype == object  # str
+
+    assert df.avail.dtype == int  # int64
+    assert df.price.dtype == object  # Decimal
+    assert df.marketplace_url.dtype == object  # str
+
+    # Assert this user has at least 1 item for sale
+    # (if they don't then change the test user.)
+    assert len(df.index) >= 1
+
+
+def test_extract_market_offers():
+    """Test extracting offers from the public marketplace url."""
+
+    test_product_url = "https://www.cardmarket.com/en/Magic/Products/Singles/Friday-Night-Magic-Promos/Aether-Hub"
+    df = extract_market_offers(test_product_url)
+    assert isinstance(df, pd.DataFrame)
+
+
+    assert df.price.dtype == object # Decimal
+    assert df.cond.dtype == object  # Decimal
+    
+    assert df.user_name.dtype == object  # str
+    assert df.user_uri.dtype == object  # str
+    assert df.user_sales.dtype == int
+
+    # Assert that there is at least 1 copy of this item for sale 
+    # (if they don't then find another item url.)
+    assert len(df.index) >= 1
+    
